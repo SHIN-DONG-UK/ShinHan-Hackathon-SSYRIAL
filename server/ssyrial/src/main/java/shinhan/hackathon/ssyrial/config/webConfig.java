d3f279760d9a9fc379a56ec2187725efb9e18f80
송명석 <67058185+sms1875@users.noreply.github.com>
2024-08-28 14:02:48 +0900

    test(server):webConfig수정

--- server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/config/webConfig.java
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/config/webConfig.java
@@ -7,13 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins("*")  // 모든 도메인 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
-                .allowedHeaders("*")  // 모든 헤더 허용
-                .allowCredentials(false);  // 자격 증명(쿠키 등)을 허용하지 않음 (필요 시 true로 설정)
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // “*“같은 와일드카드를 사용
+                .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowCredentials(true); // 쿠키 인증 요청 허용
     }
 }
