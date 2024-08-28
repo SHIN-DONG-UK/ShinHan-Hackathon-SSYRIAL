a343a17fd74ad44c224f0aba66f97e0f5b338be7
송명석 <67058185+sms1875@users.noreply.github.com>
2024-08-28 13:33:25 +0900

    fix(server):userkey추가

--- server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/model/demandDeposit/InquireDemandDepositListModel.java
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/model/demandDeposit/InquireDemandDepositListModel.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 
 /**
  * InquireDemandDepositListModel 클래스는 은행별 계좌 상품 조회 요청 및 응답 데이터를 담는 모델 클래스입니다.
@@ -27,6 +28,9 @@ public class InquireDemandDepositListModel {
   public static class Request {
     @JsonProperty("Header")
     private CommonHeaderModel.Request Header;
+
+    @NotBlank(message = "사용자 키는 필수 입력 항목입니다.")
+    private String userKey;
   }
 
   /**
