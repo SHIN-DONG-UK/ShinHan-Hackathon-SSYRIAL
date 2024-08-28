31ab70bc7a0b3b8807a41ee92fe4ea42338c8b41
신용현 <khy0728kr@naver.com>
2024-08-28 21:28:04 +0900

    feat(server) : TransactionMemoController(거래메모기능) 구현
    feat(server) : TransactionMemoController(거래메모기능) 구현

---
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/controller/TransactionMemoController.java
@@ -0,0 +1,30 @@
+package shinhan.hackathon.ssyrial.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shinhan.hackathon.ssyrial.model.ApiResponse;
+import shinhan.hackathon.ssyrial.model.transactionMemo.*;
+import shinhan.hackathon.ssyrial.service.TransactionMemoService;
+
+
+@RestController
+@RequestMapping("/api/transactionMemo")
+public class TransactionMemoController extends BaseController {
+
+  private final TransactionMemoService TransactionMemoService;
+
+  public TransactionMemoController(TransactionMemoService TransactionMemoService) {
+    this.TransactionMemoService = TransactionMemoService;
+  }
+
+
+  @PostMapping("")
+  public ResponseEntity<ApiResponse<TransactionMemoModel.Response>> transactionMemo(
+    @RequestBody TransactionMemoModel.Request request
+  ) {
+    TransactionMemoModel.Response response = TransactionMemoService.transactionMemo(
+        request.getAccountNo(), request.getTransactionUniqueNo(), request.getTransactionMemo()
+    );
+    return successResponse(response);
+  }
+}
