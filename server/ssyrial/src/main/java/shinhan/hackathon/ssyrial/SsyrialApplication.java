24fc2b9d126fed9f3e85beb73bfd7f66f2e8c952
송명석 <67058185+sms1875@users.noreply.github.com>
2024-08-28 11:53:50 +0900

    test(server):PostConstruct 시간설정
    스프링서버 시간 설정
    properties 시간 삭제

--- server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/SsyrialApplication.java
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/SsyrialApplication.java
@@ -1,12 +1,21 @@
 package shinhan.hackathon.ssyrial;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SsyrialApplication {
 
-  public static void main(String[] args) {
+    @PostConstruct
+    public void started() {
+      TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+    
+    public static void main(String[] args) {
     SpringApplication.run(SsyrialApplication.class, args);
   }
 
