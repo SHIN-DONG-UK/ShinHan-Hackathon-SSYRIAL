7dded552b63b4e5f1c3cb4cd168f50afa7896f48
신용현 <khy0728kr@naver.com>
2024-08-28 21:28:37 +0900

    fix(server) : transactionMemoService(거래메모기능) 수정
    fix(server) : transactionMemoService(거래메모기능) 수정

--- server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/service/transactionMemoService.java
+++ server/ssyrial/src/main/java/shinhan/hackathon/ssyrial/service/transactionMemoService.java
@@ -11,18 +11,18 @@ import shinhan.hackathon.ssyrial.model.CommonHeaderModel;
 import shinhan.hackathon.ssyrial.model.transactionMemo.TransactionMemoModel;
 
 @Service
-public class TransactionMemo extends ShinhanApiService {
+public class TransactionMemoService extends ShinhanApiService {
 
-  public TransactionMemo(RestTemplate restTemplate, String apiKey) {
+  public TransactionMemoService(RestTemplate restTemplate, String apiKey) {
     super(restTemplate, apiKey);
   }
 
-  public TransactionMemo.Response transactionMemo(String accountNo, String transactionUniqueNo, String transactionMemo) {
+  public TransactionMemoModel.Response transactionMemo(String accountNo, String transactionUniqueNo, String transactionMemo) {
     // 공통 헤더 생성, userKey는 필요하지 않음
     CommonHeaderModel.Request header = createCommonHeader("transactionMemo", "transactionMemo", null);
 
     // 은행 코드 조회 요청 데이터 생성
-    TransactionMemo.Request request = TransactionMemo.Request.builder()
+    TransactionMemoModel.Request request = TransactionMemoModel.Request.builder()
         .Header(header)
         .accountNo(accountNo)
         .transactionUniqueNo(transactionUniqueNo)
@@ -30,6 +30,6 @@ public class TransactionMemo extends ShinhanApiService {
         .build();
 
     // API 호출 및 응답 반환
-    return sendRequest("/edu/transactionMemo", HttpMethod.POST, request, TransactionMemo.Response.class, true);
+    return sendRequest("/edu/transactionMemo", HttpMethod.POST, request, TransactionMemoModel.Response.class, true);
   }
 }
