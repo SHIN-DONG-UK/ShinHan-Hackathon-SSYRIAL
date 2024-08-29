82f542c70379cca048c95f44670e4a6cfe09dbe2
sooyeon <20200918@sungshin.ac.kr>
2024-08-30 00:19:18 +0900

    추후 수정해야함 10.finish_member_registeration(불필요 코드 삭제)

--- client/lib/sooyeon_front/10.finish_member_registeration.dart
+++ client/lib/sooyeon_front/10.finish_member_registeration.dart
@@ -1,23 +1,5 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Member Registration',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        scaffoldBackgroundColor: Color(0xFFFCE4EC), // Light pink background
-      ),
-      home: FinishMemberRegistrationScreen(),
-    );
-  }
-}
-
+ 
 class FinishMemberRegistrationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
