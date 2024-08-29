13b28b9da33f51e48d5e4a88b550cdd2b59edf9f
VioletOokamiS <106414290+VioletOokamiS@users.noreply.github.com>
2024-08-29 09:03:14 +0900

    feat(client): Fix incorrect file-main.dart
    잘못 올라간 파일 내용 수정

--- client/lib/kimuijong/main.dart
+++ client/lib/kimuijong/main.dart
@@ -1,174 +1,110 @@
 import 'package:flutter/material.dart';
-import '../createaccountfinalcheck/1_finalcheckscreen.dart';
-import 'dart:async';
+import 'create_accout_select_category.dart'; ///가야할 새로 추가된 파일을 임포트
 
-class Ipchulgeumabiliablecheck extends StatefulWidget {
-  const Ipchulgeumabiliablecheck({super.key});
-
-  @override
-  _IpchulgeumabiliablecheckState createState() => _IpchulgeumabiliablecheckState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _IpchulgeumabiliablecheckState extends State<Ipchulgeumabiliablecheck> {
-  final StreamController<String> _streamController = StreamController<String>();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
 
   @override
-  void dispose() {
-    _streamController.close(); // Close the stream when the widget is disposed
-    super.dispose();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-  void _showStreamAlertDialog(BuildContext context) {
-    // Function to send updated messages to the stream
-    void updateMessage() {
-      // Add initial message
-      _streamController.add('한 달 이내에\n입출금 통장이\n개설되었는지\n확인하고 있어요.\n(1/2)');
+class _MyHomePageState extends State<MyHomePage> {
 
-      // Simulate message update after 3 seconds
-      Future.delayed(Duration(seconds: 3), () {
-        if (Navigator.canPop(context)) {
-          _streamController.add('기존에 소유한\n한도 제한 계좌가\n있는지\n확인하고 있어요.\n(2/2)');
-        }
-      });
-      Future.delayed(Duration(seconds: 6), () {
-        if (Navigator.canPop(context)) {
-          _streamController.add('이 계좌를\n생성할 수 있어요.');
-        }
-      });
-      Future.delayed(Duration(seconds: 10), () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => const Finalcheckscreen()),
-        );
-      });
-    }
-
-    // Show the dialog
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        // Start updating the message
-        updateMessage();
-
-        return StreamBuilder<String>(
-          stream: _streamController.stream,
-          initialData: '초기 메시지',
-          builder: (context, snapshot) {
-            return AlertDialog(
-              title: Text('알림'),
-              content: Text(snapshot.data ?? '로딩 중...'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    _streamController.close(); // Close the stream
-                  },
-                  child: Text('닫기'),
-                ),
-              ],
-            );
-          },
-        );
-      },
+  ///이 부분이 화면을 넘길 때 쓰이는 것 같음.
+  void nav_to_create_accout_select_category() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const CreateAccoutSelectCategory()),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context); // 이전 화면으로 돌아가기
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue, // 버튼 배경색
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15), // 버튼 패딩
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12), // 버튼 모서리 둥글게
-                  ),
-                ),
-                child: const Text(
-                  '돌아가기',
-                  style: TextStyle(
-                    fontSize: 18, // 글자 크기
-                    fontWeight: FontWeight.bold, // 글자 두께
-                    color: Colors.white, // 글자 색상
-                    letterSpacing: 1.5, // 글자 간격
-                  ),
-                ),
-              ),
-              SizedBox(height: 32.0),
-              _buildAccountOption("test 3 입출금 통장", "연 0.2% ~ 2.8%"),
-              SizedBox(height: 32.0),
-              Text(
-                "이 계좌를\n만들 수 있는지\n2가지 항목을\n확인해 볼게요.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: () {
-                  _showStreamAlertDialog(context);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green, // Button color
-                  padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-                child: Text(
-                  "확인하기",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ],
-          ),
+
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+
+
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            ElevatedButton(
+              onPressed: nav_to_create_accout_select_category,
+              child: const Text('계좌 생성하기'),
+            ),
+          ],
         ),
       ),
-    );
-  }
-
-  Widget _buildAccountOption(String title, String subtitle) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            subtitle,
-            style: TextStyle(
-              fontSize: 14.0,
-              color: Colors.white,
-            ),
-          ),
-        ],
-      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
