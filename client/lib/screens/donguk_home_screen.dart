9d26edf7dc8c5a751eb38cd4770c1975e5ac4fc6
신동욱 <140220959+SHIN-DONG-UK@users.noreply.github.com>
2024-08-31 14:04:23 +0900

    feat(client): donguk main screen 버튼 UI 수정

--- client/lib/screens/donguk_home_screen.dart
+++ client/lib/screens/donguk_home_screen.dart
@@ -68,9 +68,11 @@ class DongukHomeScreen extends StatelessWidget {
   Widget _buildTransfer(BuildContext context, String text) {
     return Container(
       width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 20),
       child: ElevatedButton(
-        child: Text(text, style: TextStyle(fontSize: 18)),
+        child: Text(text, style: const TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold)),
         style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.green,
           padding: EdgeInsets.symmetric(vertical: 16),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8),
